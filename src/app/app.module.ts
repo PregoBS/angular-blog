b9eec04d8a7b5d89ef84be491a7f6854c1182095
PregoBS <rodrigocherutti@hotmail.com>
2023-09-12 14:02:20 -0300

    bootstrap application configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { LargeCardComponent } from './components/large-card/large-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuTitleComponent,
+    LargeCardComponent,
+    SmallCardComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
